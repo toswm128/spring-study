c2f8d9f0f69199184bdb705514e2eccd3553566e
조민수 <chominsu10@gmail.com>
2021-08-14 23:06:47 +0900

    MVC

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.hellospring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+
+    @GetMapping("mvc")
+    public String params(@RequestParam("name") String name , Model model){
+        model.addAttribute("name",name);
+        return "params";
+    }
+}
